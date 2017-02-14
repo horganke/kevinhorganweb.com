381c362113aa84fdfa52f096a88e4e2e4ce1d2e4
Kevin Horgan <kevin.scott.horgan@gmail.com>
2017-02-14 00:34:37 -0500

    continued slider and toggle work

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 $(function(){
 
-	$("#slideshow > div:gt(0)").hide();
+	$("#slideshow-current > div:gt(0)").hide();
 
 	setInterval(function() {
-	  $('#slideshow > div:first')
+	  $('#slideshow-current > div:first')
 	    .fadeOut(1000)
 	    .next()
 	    .fadeIn(1000)
 	    .end()
-	    .appendTo('#slideshow');
+	    .appendTo('#slideshow-current');
 	}, 5000);
+
+	$("#slideshow-before > div:gt(0)").hide();
+
+	setInterval(function() {
+	  $('#slideshow-before > div:first')
+	    .fadeOut(1000)
+	    .next()
+	    .fadeIn(1000)
+	    .end()
+	    .appendTo('#slideshow-before');
+	}, 5000);
+
+	$('.switch-field').click(function() {
+	   if($('#switch_right').is(':checked')) { 
+   			$('#slideshow-current').toggleClass('hidden shown');
+   			$('#slideshow-before').toggleClass('shown hidden');
+	   }
+	});
 
 });
