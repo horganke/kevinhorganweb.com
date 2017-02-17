574459d7b9fcaeef0c4817f726fd4b175f24695f
Kevin Horgan <kevin.scott.horgan@gmail.com>
2017-02-16 23:14:26 -0500

    finished work section, onto instagram feed

--- js/main.js
+++ js/main.js
@@ -1,32 +1,94 @@
+//instafeed
+
+var feed = new Instafeed({ 
+    get: 'user',
+    userId: 398362933,
+    accessToken: '398362933.1677ed0.c82bd983d3b84f42a29e2da20e3e9225',
+    resolution: 'low_resolution',
+    limit: 50,
+    template: '<a href="{{link}}"><img src="{{image}}"><span class="caption"><p>"{{location}}"</p></span></a>',
+    filter: function(image) {
+    return image.tags.indexOf('life') >= 0;
+    },
+}); 
+feed.run();
+
 $(function(){
 
-	$("#slideshow-current > div:gt(0)").hide();
+	//nav
 
-	setInterval(function() {
-	  $('#slideshow-current > div:first')
-	    .fadeOut(1000)
-	    .next()
-	    .fadeIn(1000)
-	    .end()
-	    .appendTo('#slideshow-current');
-	}, 5000);
+    $('#menuIcon').click(function() {
+        $('main').toggleClass('overlay');
+        $('#menuIcon').toggleClass('open');
+        $('#menuIcon').toggleClass("fa-bars fa-close");
+        $('nav ul').toggleClass('nav-shown');
+    });
 
-	$("#slideshow-before > div:gt(0)").hide();
 
-	setInterval(function() {
-	  $('#slideshow-before > div:first')
-	    .fadeOut(1000)
-	    .next()
-	    .fadeIn(1000)
-	    .end()
-	    .appendTo('#slideshow-before');
-	}, 5000);
+
+    // Binding an event handler to all anchors that contain
+    // a hash (#), but not necessarily JUST a hash - like href="#"
+    // which is typically used in JS...
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+
+        // Two conditional checks
+        // First:
+        // location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        // What we're doing is replacing the first forward slash (/) in the pathname
+        // for the current location and comparing it to the link that's been clicked.
+        // So http://personalsite.com/test/link/src, which normally would have
+        // a pathname of /test/link/src would be test/link/src
+
+        // The or check (||) is to see if the link matches the current domain
+        // location.hostname == this.hostname
+
+        // Basically, we want an internal anchor for the page we're on.
+
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            // Assigning the variable target, with the hash of the link that's been clicked
+            // which is conveniently enough, a jquery selector for IDs (i.e. #hash)
+            var target = $(this.hash);
+
+            // We check the target length - basically, does the element exist?
+            // If length equals to 0, we query the DOM by name attribute. Otherwise, we just re-assign target to self.
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            // The rest is self explanatory... (animation  using the target's offset)
+            // The return false prevents default behavior
+
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - $('header').height() + 1
+                }, 500);
+                return false;
+            }
+        }
+    });
+
+    //work slideshow
+
+    $('.slideshow').each(function() {
+        $(this).children( "div:gt(0)" ).hide(); 
+    });
+
+    setInterval(function() {
+        $('.slideshow').each(function() {
+            $(this).children('div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo(this);
+        });
+
+    },  4000);
 
 	$('.switch-field').click(function() {
-	   if($('#switch_right').is(':checked')) { 
-   			$('#slideshow-current').toggleClass('hidden shown');
-   			$('#slideshow-before').toggleClass('shown hidden');
-	   }
+        if($('.switch_before').is(':checked')) { 
+            $(this).closest('article').find('.current, .before').toggleClass('hidden shown');
+        }
 	});
 
 });
